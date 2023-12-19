10a5dccfa7e5e8b1a9b1adf42642c70c53fbbc8e
tamani phiri <tamanigabriel0@gmail.com>
2023-12-19 19:34:40 +0200

    c

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,7 +1,5 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
 import { ProductsService } from './products.service';
-import { CreateProductDto } from './dto/create-product.dto';
-import { UpdateProductDto } from './dto/update-product.dto';
 
 @Controller('products')
 export class ProductsController {
