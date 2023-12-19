5ef1f6dc183c63dfd507b5a31f6cdd723efac790
tamani phiri <tamanigabriel0@gmail.com>
2023-12-19 19:40:41 +0200

    c

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -30,7 +30,10 @@ export class ProductsController {
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateProductDto: UpdateProductDto) {
+  update(
+    @Param('id') id: string,
+    @Body() updateProductDto: Prisma.ProductUpdateInput,
+  ) {
     return this.productsService.update(+id, updateProductDto);
   }
 
