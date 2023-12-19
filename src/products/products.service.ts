ff7e665a1581a276d0781945378d95e8c4efb4bc
tamani phiri <tamanigabriel0@gmail.com>
2023-12-19 19:50:40 +0200

    c

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -17,7 +17,7 @@ export class ProductsService {
   }
 
   findOne(id: number) {
-    return `This action returns a #${id} product`;
+    return this.databaseService.product.findUnique({ where: { id } });
   }
 
   update(id: number, updateProductDto: Prisma.ProductUpdateInput) {
