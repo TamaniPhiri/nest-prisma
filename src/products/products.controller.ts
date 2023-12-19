ae7b39d2c1588e0a1e4d08f9cececcf0a479f431
tamani phiri <tamanigabriel0@gmail.com>
2023-12-19 19:35:45 +0200

    c

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -8,13 +8,14 @@ import {
   Delete,
 } from '@nestjs/common';
 import { ProductsService } from './products.service';
+import { Prisma } from '@prisma/client';
 
 @Controller('products')
 export class ProductsController {
   constructor(private readonly productsService: ProductsService) {}
 
   @Post()
-  create(@Body() createProductDto: CreateProductDto) {
+  create(@Body() createProductDto: Prisma.ProductCreateInput) {
     return this.productsService.create(createProductDto);
   }
 
