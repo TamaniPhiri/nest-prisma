48dd54759be13755c14af3c504172876d25a0f24
tamani phiri <tamanigabriel0@gmail.com>
2023-12-19 19:29:13 +0200

    c

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DatabaseModule } from './database/database.module';
+import { ProductsModule } from './products/products.module';
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [DatabaseModule, ProductsModule],
   controllers: [AppController],
   providers: [AppService],
 })
